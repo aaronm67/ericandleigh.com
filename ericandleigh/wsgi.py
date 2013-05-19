7ebda2555405c2a18c96492e3765b9f5ebcc2e6c
Aaron Marasco <agm257@mail.missouri.edu>
2013-05-19 07:33:45 -0500

    fix comp error

--- ericandleigh/wsgi.py
+++ ericandleigh/wsgi.py
@@ -3,6 +3,6 @@ import sys
 sys.path.append('/var/django/ericandleigh.com')
 sys.path.append('/var/django')
 
-os.environ.setdefault("DJANGO_  SETTINGS_MODULE", "settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
