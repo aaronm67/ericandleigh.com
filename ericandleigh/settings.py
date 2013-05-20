dd0490af2e330a52f310104ebeca4910c626d78b
Aaron Marasco <agm257@mail.missouri.edu>
2013-05-20 08:37:39 -0500

    Allow serving from ericandleigh.com

--- ericandleigh/settings.py
+++ ericandleigh/settings.py
@@ -6,7 +6,7 @@ TEMPLATE_DEBUG = DEBUG
 
 
 ADMINS = (
-    # ('Your Name', 'your_email@example.com'),
+    ('Aaron Marasco', 'agm257@mail.missouri.edu'),
 )
 
 MANAGERS = ADMINS
@@ -25,7 +25,7 @@ DATABASES = {
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [ ".ericandleigh.com" ]
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
